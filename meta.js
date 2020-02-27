6d933bb33ad51487c4d92ad4f6aeeeeac6d3a93f
hm <gaohm90@gmail.com>
2020-02-27 23:59:46 +0800

    2121

--- meta.js
+++ meta.js
@@ -51,29 +51,29 @@ module.exports = {
       type: 'string',
       message: 'Author',
     },
-    build: {
-      when: 'isNotTest',
-      type: 'list',
-      message: 'Vue build',
-      choices: [
-        {
-          name: 'Runtime + Compiler: recommended for most users',
-          value: 'standalone',
-          short: 'standalone',
-        },
-        {
-          name:
-            'Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere',
-          value: 'runtime',
-          short: 'runtime',
-        },
-      ],
-    },
-    router: {
-      when: 'isNotTest',
-      type: 'confirm',
-      message: 'Install vue-router?',
-    },
+    // build: {
+    //   when: 'isNotTest',
+    //   type: 'list',
+    //   message: 'Vue build',
+    //   choices: [
+    //     {
+    //       name: 'Runtime + Compiler: recommended for most users',
+    //       value: 'standalone',
+    //       short: 'standalone',
+    //     },
+    //     {
+    //       name:
+    //         'Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere',
+    //       value: 'runtime',
+    //       short: 'runtime',
+    //     },
+    //   ],
+    // },
+    // router: {
+    //   when: 'isNotTest',
+    //   type: 'confirm',
+    //   message: 'Install vue-router?',
+    // },
     // lint: {
     //   when: 'isNotTest',
     //   type: 'confirm',
